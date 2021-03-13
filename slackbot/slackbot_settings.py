c55c06f328a298107009e7f44fcef93a7f691af2
dolcewendy <ikuma05110190@gmail.com>
2021-03-13 15:56:34 +0900

    edit setting

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-1852333648325-1855423393810-uWLsdrOT7uiPpVfAFbLvJdRN"
+API_TOKEN = "XXX"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "何言ってんだこいつ"
